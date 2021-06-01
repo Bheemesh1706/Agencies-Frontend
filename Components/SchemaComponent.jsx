332d57f2fedafbd7aa81cf9ab217dd593570ec47
Bheemesh1706 <bheemesh.18bce7170@vitap.ac.in>
2021-06-01 17:41:55 +0530

    Register form validation done

--- Components/SchemaComponent.jsx
+++ Components/SchemaComponent.jsx
@@ -1,14 +1,14 @@
 import * as yup from "yup";
 
 const RegisterSchema = yup.object().shape({
-    AgencyName: yup.string().required().min(5, 'Must be exactly 5 digits'),
-    Name: yup.string().required().min(5, 'Must be exactly 5 digits'),
-    Password : yup.string().required('Password is required'),
-    ConfirmPassword : yup.string().oneOf([yup.ref('Password'), null], 'Passwords must match'),
-    Address : yup.string().required(),
-    City : yup.string().required(),
-    Area : yup.string().required(),
-    Code : yup.string().required(),
+    AgencyName: yup.string().required().min(5).max(10).matches(/^[a-zA-Z]+$/, "Only alphabets are allowed"),
+    Name: yup.string().required().min(10).max(25).matches(/^[a-zA-Z]+$/, "Only alphabets are allowed"),
+    Password : yup.string().required('Password is required').min(8).max(15).matches(/^(?=.*[0-9])(?=.*[!@#$%^&*])(?=.*[A-Z])[a-zA-Z0-9!@#$%^&*]+$/,'Password must contain atleast one speacial charecter, digit and a capital letter'),
+    ConfirmPassword : yup.string().oneOf([yup.ref('Password'), null], 'Passwords doesn\'t match'),
+    Address : yup.string().required().min(10).max(50).matches(/^[a-zA-Z0-9\s,'-:/]+$/, "Only alphabets and special charecters like ':-/ are allowed"),
+    City : yup.string().required().min(4).max(20).matches(/^[a-zA-Z]+$/, "Only alphabets are allowed"),
+    Area : yup.string().required().min(5).max(30).matches(/^[a-zA-Z]+$/, "Only alphabets are allowed"),
+    Code : yup.string().required().min(7).max(7).matches(/^[0-9]+$/, "Must be only digits"),
   });
 
 export {RegisterSchema}; 
