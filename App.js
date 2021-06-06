542a9cd70eec5e3d80a2b022d8d02293f9fe5c5a
Bheemesh1706 <bheemesh.18bce7170@vitap.ac.in>
2021-06-06 13:27:24 +0530

    Done implementing react navigation

--- App.js
+++ App.js
@@ -4,14 +4,22 @@ import { Text, View } from 'react-native';
 import styles from './Components/StyleComponent'
 import Register from './Components/RegisterComponent'
 import Login from './Components/LoginComponent'
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+const Stack = createStackNavigator();
 
 export default function App() {
 
   return (
-    <View style={styles.container}>
-      <Login/>
-      <StatusBar style="auto" />
-    </View>
+    
+          <NavigationContainer>
+                <Stack.Navigator initialRouteName = "Register">
+                    <Stack.Screen name="Register" component= {Register}/>
+                    <Stack.Screen name = "Login" component = {Login}/>
+                </Stack.Navigator>
+          </NavigationContainer>
+    
   );
   
 }
