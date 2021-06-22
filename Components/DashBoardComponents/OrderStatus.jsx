28715c54624c1df066404e6de2562542d147b289
Bheemesh1706 <bheemesh.18bce7170@vitap.ac.in>
2021-06-22 15:58:24 +0530

    Added order and orderstatus card

---
+++ Components/DashBoardComponents/OrderStatus.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View ,Text } from 'react-native';
+
+export default function Order()
+{
+    return(<View>
+        <Text >
+         #Order Number:  {'\n'}
+         #Order Status:  {'\n'}
+         Customer Name:  {'\n'}
+         Delivery Code:  {'\n'}
+        </Text> 
+      </View>);
+}
